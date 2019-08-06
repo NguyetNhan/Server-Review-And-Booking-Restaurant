6a9cb569d6b3e5b8950fe144655e06f0a5fc3275
NguyetNhan <43900183+NguyetNhan@users.noreply.github.com>
2019-08-06 12:23:09 +0700

    update

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -11,19 +11,19 @@ app.post('/signup', async (req, res) => {
         try {
                 if (lodash.isEmpty(req.body.name)) {
                         format.error = true;
-                        format.message = 'Tên không được để trống';
+                        format.message = 'Tên không được để trống !';
                         res.json(format);
                 } else if (lodash.isEmpty(req.body.email)) {
                         format.error = true;
-                        format.message = 'Email không được để trống';
+                        format.message = 'Email không được để trống !';
                         res.json(format);
                 } else if (lodash.isEmpty(req.body.password)) {
                         format.error = true;
-                        format.message = 'Mật khẩu không được để trống';
+                        format.message = 'Mật khẩu không được để trống !';
                         res.json(format);
                 } else if (lodash.isEmpty(req.body.phone)) {
                         format.error = true;
-                        format.message = 'Số điện thoại không được để trống';
+                        format.message = 'Số điện thoại không được để trống !';
                         res.json(format);
                 } else {
                         var user = {
@@ -41,20 +41,20 @@ app.post('/signup', async (req, res) => {
                                 const result = await Model.find(options);
                                 if (result.length > 0) {
                                         format.error = true;
-                                        format.message = 'Tài khoản đã tồn tại';
-                                        format.data = result;
+                                        format.message = 'Tài khoản đã tồn tại !';
+                                        format.data = result[0];
                                         res.json(format);
                                 } else {
                                         try {
                                                 var results = await Model.create(user);
                                                 results.password = null;
                                                 format.error = false;
-                                                format.message = 'Tạo tài khoản thành công';
+                                                format.message = 'Tạo tài khoản thành công !';
                                                 format.data = results;
                                                 res.json(format);
                                         } catch (error) {
                                                 format.error = true;
-                                                format.message = 'Tạo tài khoản không thành công';
+                                                format.message = 'Tạo tài khoản không thành công !';
                                                 format.data = error;
                                                 res.json(format);
                                         }
@@ -79,7 +79,7 @@ app.post('/login', async (req, res) => {
         };
         if (lodash.isEmpty(req.body.email)) {
                 error = true;
-                message = 'Email không được để trống';
+                message = 'Email không được để trống !';
                 let result = {
                         error: error,
                         message: message,
@@ -88,7 +88,7 @@ app.post('/login', async (req, res) => {
                 res.json(result);
         } else if (lodash.isEmpty(req.body.password)) {
                 error = true;
-                message = 'Mật khẩu không được để trống';
+                message = 'Mật khẩu không được để trống !';
                 let result = {
                         error: error,
                         message: message,
@@ -103,24 +103,25 @@ app.post('/login', async (req, res) => {
                         let result = await Model.find(options);
                         if (result.length === 0) {
                                 format.error = true;
-                                format.message = 'Email không tồn tại';
+                                format.message = 'Email không tồn tại !';
                                 format.data = null;
                                 res.json(format);
                         } else {
                                 if (req.body.password === result[0].password) {
                                         format.error = false;
-                                        format.message = 'Đăng nhập thành công';
+                                        format.message = 'Đăng nhập thành công !';
                                         result[0].password = null;
-                                        format.data = result;
+                                        format.data = result[0];
                                         res.json(format);
                                 } else {
                                         format.error = true;
-                                        format.message = 'Sai mật khẩu';
+                                        format.message = 'Sai mật khẩu !';
                                         format.data = null;
                                         res.json(format);
                                 }
                         }
                 } catch (error) {
+                        console.log('error: ', error);
                         format.error = true;
                         format.message = error.message;
                         format.data = error;
